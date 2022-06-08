88001a7b60bcf8f91834ca2c88050f98952d3868
Rafael Ferreira <rafaelmf7@gmail.com>
2022-06-08 15:20:07 -0300

    style css snake-game

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -41,10 +41,10 @@ function update(event) {
 }
 
 function startGame() {
-  if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
-  if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
-  if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
-  if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+  if (snake[0].x >= 15 * box && direction == "right") direction = "down"; /*snake[0].x = 0; */
+  if (snake[0].x <= 0 && direction == "left") direction = "up"; //snake[0].x = 16 * box;
+  if (snake[0].y >= 15 * box && direction == "down") direction = "left"; //snake[0].y = 0;
+  if (snake[0].y <= 0 && direction == "up") direction = "right"; //snake[0].y = 16 * box;
 
   for (i = 1; i < snake.length; i++) {
     if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
