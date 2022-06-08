a88d3a137df3f993c756217b5e4223a2c73b095f
Rafael Ferreira <rafaelmf7@gmail.com>
2022-06-08 09:55:39 -0300

    snake movements

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -2,10 +2,13 @@ let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
+
 snake[0] = {
   x: 8 * box,
   y: 8 * box,
 };
+
+let direction = "right";
 
 function createBG() {
   context.fillStyle = "lightgreen";
@@ -19,5 +22,23 @@ function createSnake() {
   }
 }
 
-createBG();
-createSnake();
+function startGame() {
+  createBG();
+  createSnake();
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+  if (direction == "right") snakeX += box;
+  if (direction == "left") snakeX -= box;
+  if (direction == "up") snakeY -= box;
+  if (direction == "down") snakeY += box;
+
+  snake.pop();
+
+  let newHead = {
+    x: snakeX,
+    y: snakeY,
+  };
+  snake.unshift(newHead);
+}
+
+let game = setInterval(startGame, 100);
